98c990ac41a1f550241352de22bca7b392d66a02
MiraiderHZ <hilzhardi.23@gmail.com>
2024-06-11 19:22:59 +0700

    updated: cached network image, optimized image upload

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image/image.dart' as img;
 import 'package:image_picker/image_picker.dart';
 
 class StorageService {
@@ -11,7 +12,11 @@ class StorageService {
     try {
       final imageRef = ref.child(fileName);
       final imageBytes = await file.readAsBytes();
-      await imageRef.putData(imageBytes);
+
+      // Optimize the image
+      final optimizedImageBytes = await optimizeImage(imageBytes);
+
+      await imageRef.putData(optimizedImageBytes);
     } catch (e) {
       print('Could not upload file: $e');
     }
@@ -24,6 +29,34 @@ class StorageService {
     } catch (e) {
       print('Could not get file: $e');
       return null;
+    }
+  }
+
+  Future<String> getDownloadURL(String fileName) async {
+    try {
+      final imageRef = ref.child(fileName);
+      return await imageRef.getDownloadURL();
+    } catch (e) {
+      print('Could not get download URL: $e');
+      throw e;
+    }
+  }
+
+  Future<Uint8List> optimizeImage(Uint8List imageBytes) async {
+    // Decode the image
+    img.Image? image = img.decodeImage(imageBytes);
+
+    if (image != null) {
+      // Resize the image to a maximum width of 800px, preserving the aspect ratio
+      img.Image resizedImage = img.copyResize(image, width: 800);
+
+      // Compress the image as JPEG with 80% quality
+      Uint8List optimizedImageBytes = Uint8List.fromList(img.encodeJpg(resizedImage, quality: 80));
+
+      return optimizedImageBytes;
+    } else {
+      // If image decoding fails, return the original image bytes
+      return imageBytes;
     }
   }
 }
